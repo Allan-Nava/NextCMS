ae067d693c9da5464c2f3330461cdbeb4556347e
Allan Nava <allannava95@gmail.com>
2022-03-26 22:48:06 +0100

    added lib prisma client

---
+++ lib/prisma.ts
@@ -0,0 +1,27 @@
+/*
+ * File: prisma.ts
+ * Project: next-cms
+ * File Created: Saturday, 26th March 2022 10:45:39 pm
+ * Author: Allan Nava (allan.nava@hiway.media)
+ * -----
+ * Last Modified: Saturday, 26th March 2022 10:45:42 pm
+ * Modified By: Allan Nava (allan.nava@hiway.media>)
+ * -----
+ * Copyright 2022 - 2022 © 
+ */
+
+import { PrismaClient } from '@prisma/client';
+//
+let prisma: PrismaClient;
+//
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+//
+export default prisma;
+//
