5f325df333c9a7b59d6a049d8569b73ae0fac21f
Allan Nava <allannava95@gmail.com>
2022-04-19 13:59:18 +0200

    trying to fix

--- lib/hooks/dispatchHook.ts
+++ lib/hooks/dispatchHook.ts
@@ -1,6 +1,18 @@
+/*
+ * File: _appstyles.tsx
+ * Project: next-cms
+ * File Created: Sunday, 27th March 2022 9:19:11 pm
+ * Author: Allan Nava (allan.nava@hiway.media)
+ * -----
+ * Last Modified: Sunday, 27th March 2022 9:19:13 pm
+ * Modified By: Allan Nava (allan.nava@hiway.media>)
+ * -----
+ * Copyright 2022 - 2022 © 
+ */
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-//import type { RootState, AppDispatch } from '../app/store';
+import type { RootState, AppDispatch } from '../app/store'; // @toninospiderman123 need to fix this store asap
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+//
