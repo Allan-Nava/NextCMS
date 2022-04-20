a2324a29631d002a4e98b615877185021dbdeacf
Allan Nava <allannava95@gmail.com>
2022-04-20 19:41:25 +0200

    fix

--- components/pagebuilder/LayoutZone.tsx
+++ components/pagebuilder/LayoutZone.tsx
@@ -1,3 +1,15 @@
+/*
+ * File: LayoutZone.tsx
+ * Project: next-cms
+ * File Created: Tuesday, 19th April 2022 10:56:57 pm
+ * Author: Allan Nava (allan.nava@hiway.media)
+ * -----
+ * Last Modified: Wednesday, 20th April 2022 7:26:37 pm
+ * Modified By: Allan Nava (allan.nava@hiway.media>)
+ * -----
+ * Copyright 2022 - 2022 © 
+ */
+
 import React, {useMemo} from "react";
 import { useDrop } from "react-dnd";
 import { useAppSelector, useAppDispatch } from "../../lib/hooks/dispatchHook";
