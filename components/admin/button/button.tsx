76c225bec95b882e63f6520189034b9b66fc72b6
Allan Nava <allannava95@gmail.com>
2022-04-20 19:56:11 +0200

    fix gitignore

---
+++ components/admin/button/button.tsx
@@ -0,0 +1,19 @@
+/*
+ * File: button.tsx
+ * Project: next-cms
+ * File Created: Wednesday, 20th April 2022 7:53:17 pm
+ * Author: Allan Nava (allan.nava@hiway.media)
+ * -----
+ * Last Modified: Wednesday, 20th April 2022 7:53:19 pm
+ * Modified By: Allan Nava (allan.nava@hiway.media>)
+ * -----
+ * Copyright 2022 - 2022 © 
+ */
+
+import React from 'react';
+//
+const Button: React.FC<{}> = () => {
+    return (<>  </>);
+};
+export default Button;
+//
