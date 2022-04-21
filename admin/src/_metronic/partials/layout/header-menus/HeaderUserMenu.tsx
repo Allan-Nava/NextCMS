15fdc5b2a762fc0d2d5a456814aeb40425ef7b0f
Allan Nava <allannava95@gmail.com>
2022-04-21 08:12:35 +0200

    fix

--- admin/src/_metronic/partials/layout/header-menus/HeaderUserMenu.tsx
+++ admin/src/_metronic/partials/layout/header-menus/HeaderUserMenu.tsx
@@ -1,22 +1,25 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+/*
+ * File: HeaderUserMenu.tsx
+ * Project: next-admin
+ * File Created: Tuesday, 19th April 2022 11:25:16 pm
+ * Author: Allan Nava (allan.nava@hiway.media)
+ * -----
+ * Last Modified: Thursday, 21st April 2022 8:11:46 am
+ * Modified By: Allan Nava (allan.nava@hiway.media>)
+ * -----
+ * Copyright 2022 - 2022 © 
+ */
 import {FC} from 'react'
 import {shallowEqual, useSelector} from 'react-redux'
-import {UserModel} from '../../../../app/modules/auth/models/UserModel'
-import {RootState} from '../../../../setup'
-import * as auth from '../../../../app/modules/auth/redux/AuthRedux'
+//
 import {useDispatch} from 'react-redux'
 // import {Link} from 'react-router-dom'
 // import {Languages} from './Languages'
 // import {toAbsoluteUrl} from '../../../helpers'
 
 const HeaderUserMenu: FC = () => {
-  const user: UserModel = useSelector<RootState>(({auth}) => auth.user, shallowEqual) as UserModel
-
-  const dispatch = useDispatch()
-  const logout = () => {
-    dispatch(auth.actions.logout())
-  }
-
+  
   return (
     <div
       className='menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px'
@@ -26,24 +29,24 @@ const HeaderUserMenu: FC = () => {
         <div className='menu-content d-flex align-items-center px-3'>
           <div className='d-flex flex-column'>
             <div className='fw-bolder d-flex align-items-center fs-5'>
-              {user.first_name} {user.last_name}
+              
             </div>
             <a href='#' className='fw-bold text-muted text-hover-primary fs-7'>
-              {user.email}
+              
             </a>
           </div>
         </div>
       </div>
 
       {/* <Languages /> */}
-
       <div className='menu-item px-5'>
-        <a onClick={logout} className='menu-link px-5'>
+        <a className='menu-link px-5'>
           Sign Out
         </a>
       </div>
     </div>
   )
 }
-
-export {HeaderUserMenu}
+//
+export {HeaderUserMenu};
+//
