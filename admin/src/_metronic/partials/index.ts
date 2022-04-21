be1a93322cc2c95d23d9043992bed1a069c705be
Allan Nava <allannava95@gmail.com>
2022-04-21 08:24:41 +0200

    fix index.ts

--- admin/src/_metronic/partials/index.ts
+++ admin/src/_metronic/partials/index.ts
@@ -1,3 +1,15 @@
-export * from './layout/header-menus/HeaderUserMenu'
+/*
+ * File: index.ts
+ * Project: next-admin
+ * File Created: Tuesday, 19th April 2022 11:25:16 pm
+ * Author: Allan Nava (allan.nava@hiway.media)
+ * -----
+ * Last Modified: Thursday, 21st April 2022 8:24:36 am
+ * Modified By: Allan Nava (allan.nava@hiway.media>)
+ * -----
+ * Copyright 2022 - 2022 © 
+ */
 
-export * from './modals/multi-media/MultiMedia'
+//
+export * from './layout/header-menus/HeaderUserMenu'
+//
