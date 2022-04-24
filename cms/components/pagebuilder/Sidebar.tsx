05a78574a18a443a637f6af751028b0bd6b387d1
Allan Nava <allannava95@gmail.com>
2022-04-24 15:46:43 +0200

    changed sidebar stuff

--- cms/components/pagebuilder/Sidebar.tsx
+++ cms/components/pagebuilder/Sidebar.tsx
@@ -4,14 +4,13 @@
  * File Created: Monday, 18th April 2022 10:55:41 pm
  * Author: Allan Nava (allan.nava@hiway.media)
  * -----
- * Last Modified: Tuesday, 19th April 2022 11:31:45 pm
+ * Last Modified: Sunday, 24th April 2022 3:43:11 pm
  * Modified By: Allan Nava (allan.nava@hiway.media>)
  * -----
  * Copyright 2022 - 2022 © 
  */
-
 import React from 'react'
-import Link from 'next/link';
+//import Link from 'next/link';
 import { PageComponent } from '../../lib/types/page';
 import DraggableCard from './DraggableCard';
 //
@@ -21,7 +20,7 @@ const Sidebar: React.FC<{components: PageComponent[]}> = ({components}) => {
 		<div className="d-flex flex-column flex-shrink-0 p-3 bg-light">
 			{/* href="/"  */}
 			<div className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-				<span className="fs-4">Sidebar</span>
+				<span className="fs-4">Sidebar | Components</span>
 			</div>
 			{
 				components.map((item, index) => <DraggableCard key={`draggable-${item.name}-${index}`} component={item}/>)
