2a8daef7f6f7912991414fbe15ea9e0da89baa6d
Allan Nava <allannava95@gmail.com>
2022-04-27 08:09:04 +0200

    fix api users

--- cms/pages/api/user/index.ts
+++ cms/pages/api/user/index.ts
@@ -1,10 +1,11 @@
+import { userRepo } from './../../../lib/helpers/user-repo';
 /*
  * File: index.ts
  * Project: next-cms
- * File Created: Tuesday, 5th April 2022 9:00:36 pm
+ * File Created: Friday, 22nd April 2022 7:48:35 pm
  * Author: Allan Nava (allan.nava@hiway.media)
  * -----
- * Last Modified: Tuesday, 5th April 2022 9:00:38 pm
+ * Last Modified: Wednesday, 27th April 2022 7:58:00 am
  * Modified By: Allan Nava (allan.nava@hiway.media>)
  * -----
  * Copyright 2022 - 2022 © 
@@ -12,17 +13,35 @@
 //
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../lib/prisma';
+import { UserCreateNextApiRequest } from '../../../lib/types/request/user-request';
 //
 //
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-    // need to add the filters
-    try{
-        const data = await prisma.user.findMany()
-        res.json(data);
-    } catch (error) {
-        console.error("error ", error );
-        // expected output: ReferenceError: nonExistentFunction is not defined
-        // Note - error messages will vary depending on browser
+    console.log("req ", req, "res ", res);
+    // 
+    switch (req.method) {
+        case 'GET':
+            return getUsers();
+        case 'POST':
+            return createUser(req);
+        default:
+            return res.status(405).end(`Method ${req.method} Not Allowed`)
     }
+    //
+    async function getUsers() {
+        const users = await userRepo.getAll();
+        return res.status(200).json(users);
+    }
+    //
+    async function createUser(req : UserCreateNextApiRequest) {
+        try {
+            console.log("req.body ", req.body);
+            //let page = await pagesRepo.create(req.body);
+            return res.status(200).json({});
+        } catch (error) {
+            return res.status(400).json({ message: error });
+        }
+    }
+    //
 };
 //
