d33dee33202a6c479f8b9669e102b7c4c2026f20
Allan Nava <allannava95@gmail.com>
2022-05-07 16:34:10 +0200

    added lib core nextcms

---
+++ packages/core/nextcms/lib/NextCMS.js
@@ -0,0 +1,18 @@
+/*
+ * File: NextCMS.js
+ * Project: @nextcms/nextcms
+ * File Created: Saturday, 7th May 2022 4:33:19 pm
+ * Author: Allan Nava (allan.nava@hiway.media)
+ * -----
+ * Last Modified: Saturday, 7th May 2022 4:33:21 pm
+ * Modified By: Allan Nava (allan.nava@hiway.media>)
+ * -----
+ * Copyright 2022 - 2022 © 
+ */
+//
+//
+class NextCMS {
+    constructor(opts = {}) {
+      //destroyOnSignal(this);
+    }
+}
