369ddca7e02ae25d701c01e1af154de8e75a8718
Allan Nava <allannava95@gmail.com>
2022-05-09 10:13:09 +0200

    create generators

---
+++ packages/generators/generators/README.md
@@ -0,0 +1,4 @@
+# @nextcms/generators 
+
+
+
---
+++ packages/generators/generators/lib/plopfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const pluralize = require('pluralize');
+
+const generateController = require('./plops/controller');
+const generateContentType = require('./plops/content-type');
+//
+module.exports = plop => {
+  // Plop config
+  plop.setWelcomeMessage('Strapi Generators');
+  plop.addHelper('pluralize', text => pluralize(text));
+
+};
